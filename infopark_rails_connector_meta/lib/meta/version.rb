8347711211bc8de2a3bfd183e1e8066856383f8c
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2012-12-12 13:36:30 +0100

    Corrected 1.5.1 release

--- infopark_rails_connector_meta/lib/meta/version.rb
+++ infopark_rails_connector_meta/lib/meta/version.rb
@@ -1,5 +1,5 @@
 module RailsConnector
   module Meta
-    VERSION = "1.5.0"
+    VERSION = "1.5.1"
   end
 end
--- infopark_reactor_migrations/lib/reactor/migrations/version.rb
+++ infopark_reactor_migrations/lib/reactor/migrations/version.rb
@@ -1,5 +1,5 @@
 module Reactor
   module Migrations
-    VERSION = "1.5.0"
+    VERSION = "1.5.1"
   end
 end
