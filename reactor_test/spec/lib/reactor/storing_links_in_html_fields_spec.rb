c3b0bb404c8be92493edb72ac4ef0b6564637b09
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2014-10-31 09:27:48 +0100

    Update storing links in html fields spec

--- reactor_test/spec/lib/reactor/storing_links_in_html_fields_spec.rb
+++ reactor_test/spec/lib/reactor/storing_links_in_html_fields_spec.rb
@@ -12,15 +12,73 @@ describe "in-content link persisting" do
 
   shared_examples "persistent link serializer" do
     before :all do
-      @sure_object = Obj.create!(:obj_class => 'PlainObjClass', :name => 'object_sure_to_exist', :permalink => 'object_sure_to_exist', :parent => '/')
-      @sure_image = Obj.create!(:obj_class => 'image', :name => 'image_sure_to_exist', :permalink => 'image_sure_to_exist', :parent => '/')
+      @sure_object = Obj.find_by_path('/object_sure_to_exist')
+      @sure_image = Obj.find_by_path('/image_sure_to_exist')
+      @sure_object.update_attributes!(permalink: 'object_sure_to_exist') if @sure_object.permalink != 'object_sure_to_exist'
+      @sure_image.update_attributes!(permalink: 'image_sure_to_exist') if @sure_image.permalink != 'image_sure_to_exist'
+
+      @sure_object2 = Obj.create!(:obj_class => 'PlainObjClass', :name => 'object_sure_to_exist2', :parent => '/')
+      @sure_image2 = Obj.create!(:obj_class => 'image', :name => 'image_sure_to_exist2', :parent => '/')
     end
     after :all do
-      @sure_object.destroy
-      @sure_image.destroy
+      @sure_object2.destroy
+      @sure_image2.destroy
     end
 
     context "given a link to existing obj" do
+      context "with paths" do
+        it "stores an internal link" do
+          obj.send(:"#{attr}=", %Q|<a href="/object_sure_to_exist2">link</a> text|)
+          obj.save!
+          obj.text_links.first.destination_object.path.should eq('/object_sure_to_exist2')
+          Obj.find(obj.id).send(attr).should match(/internallink:/)
+        end
+
+
+        context "from html-safe string" do
+          it "stores an internal link" do
+            obj.send(:"#{attr}=", %Q|<a href="/object_sure_to_exist2">link</a> text|.html_safe)
+            obj.save!
+            obj.text_links.first.destination_object.path.should eq('/object_sure_to_exist2')
+            Obj.find(obj.id).send(attr).should match(/internallink:/)
+          end
+        end
+
+        context "given fully qualified url as image source" do
+          it "stores the url" do
+            obj.send(:"#{attr}=", %Q|<img src="http://www.page.com/object_sure_to_exist2"> text|.html_safe)
+            obj.save!
+            obj.text_links.first.url.should eq('http://www.page.com/object_sure_to_exist2')
+          end
+        end
+
+        context "given fully qualified url" do
+          it "stores the url" do
+            obj.send(:"#{attr}=", %Q|<a href="http://www.page.com/object_sure_to_exist2">link</a> text|.html_safe)
+            obj.save!
+            obj.text_links.first.url.should eq('http://www.page.com/object_sure_to_exist2')
+          end
+        end
+
+        context "given an image tag with existing obj as source" do
+          it "stores an internal link" do
+            obj.send(:"#{attr}=", %Q|<img src="/image_sure_to_exist2">|)
+            obj.save!
+            obj.text_links.first.destination_object.path.should eq('/image_sure_to_exist2')
+            Obj.find(obj.id).send(attr).should match(/internallink:/)
+          end
+        end
+
+        context "given a HTML5 image tag with existing obj as source" do
+          it "stores an internal link" do
+            obj.send(:"#{attr}=", %Q|<img src="/image_sure_to_exist2" />|)
+            obj.save!
+            obj.text_links.first.destination_object.path.should eq('/image_sure_to_exist2')
+            Obj.find(obj.id).send(attr).should match(/internallink:/)
+          end
+        end
+      end
+
       context "with permalinks" do
         it "stores an internal link" do
           obj.send(:"#{attr}=", %Q|<a href="/object_sure_to_exist">link</a> text|)
