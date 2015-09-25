d79cf8f29174c1cc3083cebc0751c3baa5d9f43e
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2015-09-25 02:08:45 +0200

    Drastically improve start-up performance

--- infopark_reactor/app/models/rails_connector/blob_mapping.rb
+++ infopark_reactor/app/models/rails_connector/blob_mapping.rb
@@ -8,5 +8,9 @@ module RailsConnector
     def self.get_fingerprint(name)
       find_by_blob_name(name).fingerprint
     end
+
+    def self.get_fingerprint_map(blob_names)
+      Hash[self.where(:blob_name => blob_names).select([:blob_name, :fingerprint]).map {|b| [b.blob_name, b.fingerprint] }]
+    end
   end
 end
--- infopark_reactor/lib/rails_connector/meta/eager_loader.rb
+++ infopark_reactor/lib/rails_connector/meta/eager_loader.rb
@@ -1,5 +1,6 @@
 # -*- encoding : utf-8 -*-
 require 'singleton'
+require 'set'
 
 module RailsConnector
   module Meta
@@ -21,22 +22,69 @@ module RailsConnector
           RailsConnector::ObjClass.includes(:custom_attributes_raw).all.each do |obj_class|
             @obj_classes[obj_class.name] = obj_class
           end
+          preload_attribute_blobs
         end
       end
 
       def obj_class(name)
         name = name.to_s
         if !@obj_classes.fetch(name, nil).nil?
-          # puts "EagerLoader: I've already loaded it: #{name}"
           @obj_classes[name]
         else
-          # puts "EagerLoader: NO HAVE: #{name}"
+          # TODO: preload_attribute_blobs for obj_class
           @obj_classes[name] ||= RailsConnector::ObjClass.find_by_obj_class_name(name)
         end
       end
 
       def forget_obj_class(name)
         @obj_classes.delete(name.to_s)
+      end
+
+      protected
+      def preload_attribute_blobs
+        attribute_names = Set.new
+        @obj_classes.each do |_, obj_class|
+          obj_class.custom_attributes.each do |attribute_name, _|
+            attribute_names << attribute_name
+          end
+        end
+
+        blob_names = attribute_names.map {|attribute_name| "#{attribute_name}.jsonAttributeDict" }
+        # Fiona >= 6.8
+        if RailsConnector::BlobMapping.exists?
+          blob_names               = attribute_names.map {|attribute_name| "#{attribute_name}.jsonAttributeDict" }
+          fingerprint_map          = RailsConnector::BlobMapping.get_fingerprint_map(blob_names)
+          blob_fingerprints        = fingerprint_map.values
+          # NOTE: this is correct! 
+          blobs                    = RailsConnector::Blob.where(:blob_name => blob_fingerprints).to_a
+          blob_map                 = Hash[blobs.map {|b| [b.blob_name, b]}]
+
+          @obj_classes.each do |_, obj_class|
+            obj_class.custom_attributes.each do |_, attribute|
+              blob_name   = "#{attribute.name}.jsonAttributeDict"
+              fingerprint = fingerprint_map[blob_name]
+              blob        = blob_map[fingerprint]
+
+              next unless blob && blob.blob_data?
+              attribute.instance_variable_set(:@blob_data, ::JSON.parse(blob.blob_data))
+            end
+          end
+        # Fiona = 6.7
+        else
+          blob_names               = attribute_names.map {|attribute_name| "#{attribute_name}.jsonAttributeDict" }
+          blobs                    = RailsConnector::Blob.where(:blob_name => blob_names).to_a
+          blob_map                 = Hash[blobs.map {|b| [b.blob_name, b]}]
+
+          @obj_classes.each do |_, obj_class|
+            obj_class.custom_attributes.each do |_, attribute|
+              blob_name   = "#{attribute.name}.jsonAttributeDict"
+              blob        = blob_map[blob_name]
+
+              next unless blob && blob.blob_data?
+              attribute.instance_variable_set(:@blob_data, ::JSON.parse(blob.blob_data))
+            end
+          end
+        end
       end
     end
   end
