c0fb012e9589359b9c0407b3f8911f27bdc44c27
Anton Mezin <anton.mezin@infopark.de>
2016-08-16 11:42:45 +0200

    small changes that allow the usage of editorial group

--- infopark_reactor/lib/reactor/cm/editorial_group.rb
+++ infopark_reactor/lib/reactor/cm/editorial_group.rb
@@ -4,11 +4,13 @@ require 'reactor/cm/group'
 module Reactor
 
   module Cm
-    
+
     # The EditorialGroup class respects the user management configured in the content manager and
     # handles all editorial groups. See @Group for further details.
     class EditorialGroup < Group
-      
+
+      primary_key :name
+
       protected
 
       # Overwritten method from +Group+.
@@ -17,7 +19,7 @@ module Reactor
       end
 
     end
-    
+
   end
-  
+
 end
