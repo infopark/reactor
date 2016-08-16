af85408c1a23aab793c790d2de5b02b2b02a6a71
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2016-08-16 14:58:59 +0200

    Add specs for group classes

---
+++ reactor_test/spec/lib/reactor/cm/group_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'Reactor groups' do
+  before { Reactor::Cm::Group.get('testingGroup').delete! rescue nil }
+  after  { Reactor::Cm::Group.get('testingGroup').delete! rescue nil }
+
+  before { Reactor::Cm::Group.create({
+    name: 'testingGroup', users: ['not_root'],
+    real_name: 'THE REAL NAME OF THE TESTING GROUP', owner: 'root'
+  })}
+
+  subject { Reactor::Cm::Group.get('testingGroup') }
+
+  specify do
+    expect(subject.user?('root')).to be_false
+    expect(subject.user?('not_root')).to be_true
+    expect(subject.name).to eq('testingGroup')
+    expect(subject.owner).to eq('root')
+    expect(subject.display_title).to eq('THE REAL NAME OF THE TESTING GROUP')
+  end
+
+  specify do
+    expect(Reactor::Cm::Group.all).to include('testingGroup')
+  end
+
+  describe 'Group proxy' do
+    subject { Reactor::Cm::EditorialGroup.get('testingGroup') }
+
+    specify do
+      expect(subject.user?('root')).to be_false
+      expect(subject.user?('not_root')).to be_true
+      expect(subject.name).to eq('testingGroup')
+      expect(subject.owner).to eq('root')
+      expect(subject.display_title).to eq('THE REAL NAME OF THE TESTING GROUP')
+    end
+
+    specify do
+      expect(Reactor::Cm::EditorialGroup.all).to include('testingGroup')
+    end
+  end
+end
