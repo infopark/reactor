c3e4efdd67354083d9f1155e0e6011ca776d8bba
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2016-11-08 14:26:33 +0100

    Improved session persistence spec

--- reactor_test/config/routes.rb
+++ reactor_test/config/routes.rb
@@ -3,6 +3,8 @@ ReactorTest::Application.routes.draw do
   controller 'test' do
     get 'test' => :test, :as => :update_ll
   end
+
+  resource :login, only: [:show, :create]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- reactor_test/spec/requests/rssession_persistance_spec.rb
+++ reactor_test/spec/requests/rssession_persistance_spec.rb
@@ -1,28 +1,36 @@
 require 'spec_helper'
 
 describe "RSession persistance" do
-  it "returns empty response without user" do
-    get "/login"
-    expect(response.body).to be_blank
-  end
+  [:marshal, :hybrid, :json].each do |serialization_scheme|
+    context "with #{serialization_scheme} cookie serialization" do
+      before do
+        Rails.application.config.action_dispatch.cookies_serializer = :serialization_scheme
+      end
 
-  context "with root user" do
-    it "persists the user in the session" do
-      post "/login", user_name: "root"
-      expect(response).to be_ok
+      it "returns empty response without user" do
+        get "/login"
+        expect(response.body).to be_blank
+      end
 
-      get "/login"
-      expect(response.body).to eq("root")
-    end
-  end
+      context "with root user" do
+        it "persists the user in the session" do
+          post "/login", user_name: "root"
+          expect(response).to be_ok
 
-  context "with non_root user" do
-    it "persists the user in the session" do
-      post "/login", user_name: "non_root"
-      expect(response).to be_ok
+          get "/login"
+          expect(response.body).to eq("root")
+        end
+      end
 
-      get "/login"
-      expect(response.body).to eq("non_root")
+      context "with non_root user" do
+        it "persists the user in the session" do
+          post "/login", user_name: "non_root"
+          expect(response).to be_ok
+
+          get "/login"
+          expect(response.body).to eq("non_root")
+        end
+      end
     end
   end
 end
