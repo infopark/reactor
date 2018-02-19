8d4784da4dd2d29a15f797768638569665f4e8da
Anton Mezin <anton.mezin@infopark.de>
2018-02-19 15:46:09 +0100

    moved to fiona connector

--- infopark_reactor/app/models/rails_connector/abstract_model.rb
+++
@@ -1,7 +0,0 @@
-module RailsConnector
-  AbstractModel = begin
-                  ::RailsConnector::InfoparkBase
-                rescue NameError
-                  ::RailsConnector::CmsBaseModel
-                end
-end
