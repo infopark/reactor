db49843bcfa5f777880e3ca80f57a9a8558858a1
Anton Mezin <anton.mezin@infopark.de>
2018-03-02 11:05:32 +0100

    can run engine also with scrivito

--- infopark_reactor/lib/reactor/engine.rb
+++ infopark_reactor/lib/reactor/engine.rb
@@ -2,14 +2,14 @@
 module Reactor
 
   class Engine < Rails::Engine
-    if ::Rails::VERSION::MAJOR == 5
-      config.after_initialize do
-        if defined?(Obj) && Obj <= RailsConnector::BasicObj
-        else
-          raise "Reactor can be only used with FionaConnector. Please define class Obj in your application"
-        end
-      end
-    end
+    # if ::Rails::VERSION::MAJOR == 5
+    #   config.after_initialize do
+    #     if defined?(Obj) && Obj <= RailsConnector::BasicObj
+    #     else
+    #       raise "Reactor can be only used with FionaConnector. Please define class Obj in your application"
+    #     end
+    #   end
+    # end
 
     rake_tasks do
       load "tasks/cm_migrate.rake"
