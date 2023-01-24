dd97d9a106fe0420b4e24f4a70214443eef454ae
Lucca Kaiser <lucca.kaiser@justrelate.com>
2023-01-24 15:52:58 +0100

    Adjust used python version in travis docker

--- reactor_test/travis/docker_login.sh
+++ reactor_test/travis/docker_login.sh
@@ -1,6 +1,6 @@
 pyenv versions
-pyenv local 3.7.6
+pyenv local 3.7
 pyenv exec pip3 install awscli --upgrade --user
 export PATH=$PATH:$HOME/.local/bin
 pyenv exec aws ecr get-login --no-include-email | bash
-pyenv local 2.7.17
+pyenv local 2.7
